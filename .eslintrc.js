172e13775af11505295e0fc7bdbb45333d504e30
cnilton <conexa.caio.barroso@conexasaude.com.br>
2021-02-16 11:30:19 -0300

    fixed linting and dependency errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
       files: ['*.ts', '*.tsx'], // Your TypeScript files extension
       parserOptions: {
         project: ['./tsconfig.json'], // Specify it only for TypeScript files
+        createDefaultProgram: true,
       },
     },
   ],
